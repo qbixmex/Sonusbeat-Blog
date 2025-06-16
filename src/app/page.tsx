afc7fe1a74e404ccec375bcff246a98298869e3e
Daniel González Briseño <qbixmex@gmail.com>
2025-06-15 21:39:31 -0600

    ShadCN Installed and Light and Dark Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import { Button } from "@/components/ui/button";
+
+import { Heart } from 'lucide-react';
+
 const HomePage = () => {
   return (
-    <div className="my-20 mx-5 md:mx-20 lg:mx-40">
-      <h1 className="text-5xl font-semibold text-center mb-10">Sonusbeat Blog</h1>
+    <div className="container mx-auto px-5 lg:px-0 py-10">
+      <h1 className="text-primary text-5xl font-semibold text-center mb-10">Sonusbeat Blog</h1>
 
-      <h2 className="text-3xl font-semibold mb-2">Bienvenidos al Blog de Sonusbeat</h2>
+      <h2 className="text-secondary-foreground text-3xl font-semibold mb-2">Bienvenidos al Blog de Sonusbeat</h2>
 
-      <p className="text-gray-200">En este blog encontrarás noticias relevantes con la producción de música electrónica.</p>
+      <p className="text-gray-500 italic mb-5">En este blog encontrarás noticias relevantes con la producción de música electrónica.</p>
+
+      <div className="flex justify-end">
+        <Button className="text-gray-200">
+          Like <Heart strokeWidth={3} />
+        </Button>
+      </div>
     </div>
   );
 };
