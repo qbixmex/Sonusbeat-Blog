57f4fcdc6e09447b6a3eea52ad22bb2c0c5af7ac
Daniel González Briseño <qbixmex@gmail.com>
2025-08-03 23:36:10 -0600

    Category Articles
    - Display category articles on "https://domain/language/category_name" url

--- src/app/[locale]/[category]/page.tsx
+++ src/app/[locale]/[category]/page.tsx
@@ -1,12 +1,53 @@
-import { use, type FC } from 'react';
+import { type FC, use } from 'react';
+import { Metadata } from 'next';
 import PublicLayout from '@/app/(public)/public.layout';
 import MainContainer from '@/components/main-container.component';
 import { useTranslations } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
 import ArticlesList from '../../(public)/components/article-list.component';
 import fetchPublicArticlesAction from '../../(public)/(actions)/fetch-articles.action';
+import { getTranslations } from 'next-intl/server';
+import fetchPublicCategoryAction from '../../(public)/(actions)/fetch-category.action';
 
-export const dynamic = 'force-dynamic';
+type MetadataProps = {
+  params: Promise<{
+    locale: string;
+    category: string;
+  }>;
+};
+
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
+
+export const generateMetadata = async ({ params }: MetadataProps): Promise<Metadata> => {
+  const { locale, category: categorySlug } = await params;
+
+  const { category } = await fetchPublicCategoryAction(categorySlug);
+
+  const categoryName = category?.translations.find(t => t.language === locale)?.name ?? "no category";
+  const categoryTranslation = category?.translations.find((t) => t.language === locale);
+  const translation = await getTranslations({ locale });
+  const seoTitle = translation('CategoryPage.seoTitle', { category: categoryName });
+  const seoDescription = translation('CategoryPage.seoDescription', { category: categoryName });
+
+  return {
+    title: seoTitle,
+    description: seoDescription,
+    robots: {
+      index: true,
+      follow: true,
+    },
+    // social media
+    openGraph: {
+      url: `${siteUrl}/${locale}/${categoryTranslation?.slug}`,
+      title: seoTitle,
+      description: seoDescription,
+      type: "article",
+      siteName: translation('SiteName'),
+      locale,
+      images: [`${siteUrl}/images/sonusbeat-logo.png`],
+    },
+  };
+};
 
 type Props = Readonly<{
   params: Promise<{
@@ -15,7 +56,9 @@ type Props = Readonly<{
   }>;
 }>;
 
-const CategoryArticules: FC<Props> = ({ params }) => {
+export const dynamic = 'force-dynamic';
+
+const CategoryArticles: FC<Props> = ({ params }) => {
   const { locale, category } = use(params);
 
   const translate = useTranslations('CategoryPage');
@@ -48,7 +91,6 @@ const CategoryArticules: FC<Props> = ({ params }) => {
       </MainContainer>
     </PublicLayout>
   );
-
 };
 
-export default CategoryArticules;
+export default CategoryArticles;
